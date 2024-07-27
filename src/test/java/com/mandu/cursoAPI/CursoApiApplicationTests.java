4c0db1a1e01fb3bfde353366e0da1fbc59305d62
Gustavo Mandu <gmandu10@gmail.com>
2024-07-27 01:40:01 -0300

    Primeiro Projeto

---
+++ src/test/java/com/mandu/cursoAPI/CursoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mandu.cursoAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
